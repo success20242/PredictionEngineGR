0145bebf7bb3db467b858fbb3f963bf5e8938d32
Onyekachi Chukwunyere Ebosi <ebosisuccess@gmail.com>
2026-04-23 19:03:29 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,22 @@ import path from 'path'
 
 export default defineConfig({
   logLevel: 'error',
+
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+
+  define: {
+    // This ensures process.env doesn't break in frontend code
+    'process.env': {}
+  }
 })

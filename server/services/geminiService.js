4aa445c44dba872096118f9c8c28e3303da7c357
Onyekachi Chukwunyere Ebosi <ebosisuccess@gmail.com>
2026-04-24 12:08:48 +0400

    Update geminiService.js

--- server/services/geminiService.js
+++ server/services/geminiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const GEMINI_URL =
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent";
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
 
 export const callGemini = async (prompt) => {
   try {
